9b913aead504a4089fbe560b1d44dd68fe73a0bf
jlemmett <jlemmetti@gmail.com>
2014-09-07 21:11:41 +0300

    Initial commit - very much work in progress

---
+++ src/om_slider/core.cljs
@@ -0,0 +1,45 @@
+(ns om-slider.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
+
+(enable-console-print!)
+
+(def app-state
+  (atom
+    {}))
+
+(defn handle-slider-change [e owner state]
+  (let [value (.. e -target -value)]
+    (print value)
+    (om/set-state! owner :value value)))
+
+(defn slider [app owner]
+
+  (reify
+
+    om/IInitState
+    (init-state [_]
+      {:value 0})
+
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+      (dom/input #js {:type "range" :min -100 :max 100 :step 0.1 :value (:value state)
+                      :onInput #(handle-slider-change % owner state)
+                      :onChange #(handle-slider-change % owner state)} nil)
+
+      (dom/span nil (:value state))))
+
+
+    )
+  )
+
+
+(om/root
+  slider
+  app-state
+  {:target (. js/document (getElementById "slider"))})
