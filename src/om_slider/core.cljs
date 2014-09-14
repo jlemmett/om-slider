05713a61a064886b5e74393253e2939bd826ae96
Janne <jlemmetti@gmail.com>
2014-09-14 21:32:16 +0300

    Trying get multiple sliders with separate states to work, does not work yet

--- src/om_slider/core.cljs
+++ src/om_slider/core.cljs
@@ -10,36 +10,50 @@
 
 (def app-state
   (atom
-    {}))
+   {:sliders [{:value 0}
+    {:value -25}
+    {:value 50}]
+    }))
 
 (defn handle-slider-change [e owner state]
   (let [value (.. e -target -value)]
+
     (om/set-state! owner :value value)))
 
-(defn slider [app owner]
+(defn slider [slider owner]
 
   (reify
 
-    om/IInitState
-    (init-state [_]
-      {:value 0})
-
     om/IRenderState
     (render-state [this state]
-      (dom/div nil
-      (dom/input #js {:type "range" :min -100 :max 100 :step 0.1 :value (:value state)
-                      :onInput #(handle-slider-change % owner state)
-                      :onChange #(handle-slider-change % owner state)
-                      :className "slider"} nil)
+                  (dom/li nil
+                          (dom/input #js {:type "range" :min -100 :max 100 :step 0.1 :value (:value slider)
+                                          :onInput #(handle-slider-change % owner slider)
+                                          :onChange #(handle-slider-change % owner slider)
+                                          :className "slider"} nil)
 
-      (dom/span nil (:value state))))
+                          (dom/span nil (:value slider))))
 
 
     )
   )
 
 
+(defn sliders-view [app owner]
+  (reify
+
+    om/IRenderState
+    (render-state [this state]
+                  (dom/div nil
+                           (dom/h2 nil "Sliders")
+                           (apply dom/ul nil
+                                  (om/build-all slider (:sliders app)))))))
+
+
 (om/root
-  slider
+  sliders-view
   app-state
   {:target (. js/document (getElementById "slider"))})
+
+
+(print @app-state)
