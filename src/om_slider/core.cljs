155f522090eadaeee7eabce040aee8b5b7727752
Janne <jlemmetti@gmail.com>
2014-09-22 21:10:17 +0300

    Trying to replace the native range in puts with noUiSlider, work in progress

--- src/om_slider/core.cljs
+++ src/om_slider/core.cljs
@@ -16,9 +16,9 @@
   (atom
    {:sliders
     [
-    {:value 0}
-    {:value -25}
-    {:value 50}
+    {:id 1 :value 0}
+    {:id 2 :value -25}
+    {:id 3 :value 50}
      ]}))
 
 
@@ -30,21 +30,42 @@
 
 (defcomponent slider [slider owner]
 
-    (render-state [this state]
-                  (dom/li nil
-                          (dom/input #js {:type "range" :min -100 :max 100 :step 0.1 :value (:value slider)
-                                          :onInput #(handle-slider-change % slider owner)
-                                          :onChange #(handle-slider-change % slider owner)
-                                          :className "slider"} nil)
+      (render [_]
 
-                          (dom/span nil (:value slider)))))
+              (dom/div nil nil)
+
+              )
+
+      (did-mount [state]
+
+
+                 (let [$slider-element ($ (.getDOMNode owner))
+                       parameters #js {:start (:value slider)
+                                                     :range #js {"min" #js [0] "max" #js [100]}
+                                                     :step 10
+                                                     :format (js/wNumb #js {:mark "," :decimals 1})}]
+
+
+
+                   (.noUiSlider $slider-element parameters)))
+
+;;     (render-state [this state]
+;;                   (dom/li nil
+;;                           (dom/input #js {:type "range" :min -100 :max 100 :step 0.1 :value (:value slider)
+;;                                           :onInput #(handle-slider-change % slider owner)
+;;                                           :onChange #(handle-slider-change % slider owner)
+;;                                           :className "slider"} nil)
+
+;;                           (dom/span nil (:value slider))))
+
+  )
 
 
 (defcomponent sliders-view [app owner]
   (render-state [this state]
                 (dom/div nil
                          (dom/h2 nil "Sliders")
-                         (apply dom/ul nil
+                         (apply dom/div nil
                                 (om/build-all slider (:sliders app))))))
 
 (defcomponent state-view [app owner]
@@ -64,8 +85,7 @@
   {:target (. js/document (getElementById "state"))})
 
 
-(def $nouislider ($ :#nouislider))
-(.noUiSlider $nouislider #js {:start [20, 80] :range #js {:min #js [0] :max #js [100]}})
+
 
 
 
